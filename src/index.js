fa366c84c0bdd63e40f6ad59f6fa2445fa43c08e
Carlo10K <carlosortegadiaz50@gmail.com>
2022-11-08 15:16:33 -0600

    fix bug

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDom from 'react-dom';
 import App from './components/App';
 import './styles/global.scss';
 
-ReactDom.render(<App/>, document.getElementById('app'));
+ReactDom.render(<App />, document.getElementById('app'));
